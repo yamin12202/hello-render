285c1b0e65ae0ccd59fd22535e7f3321b9905be7
Yamin <yamin12202@example.com>
2025-06-26 11:08:55 +0530

    Fix duplicate code and flask typo

--- main.py
+++ main.py
@@ -2,15 +2,9 @@ from flask import Flask
 
 app = Flask(__name__)
 
-
 @app.route("/")
 def home():
-    return "Hello, Render!"
+    return "Hello, world from Render!"  # You can change this message
 
 if __name__ == "__main__":
     app.run()
-
-app=flask(__name__)
-@app.route('/')
-def hello():
-    return"hello,world from render!"
